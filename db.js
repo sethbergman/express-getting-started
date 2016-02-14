645f10789cb5994c4cc36553f862f9eadc91fe57
Seth Bergman <seth.atxwebs@gmail.com>
2016-02-14 17:35:29 -0600

    testing mongoose connection reqs

--- db.js
+++ db.js
@@ -1,16 +1,8 @@
-var uri = 'mongodb://localhost:27017/test';
+var express  = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
 
-var _ = require('lodash');
-var mongoose = require('mongoose');
-mongoose.connect(uri);
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-  console.log('db connected');
-});
-
-var userSchema = mongoose.Schema({
+userSchema = new mongoose.Schema({
   username: String,
   gender: String,
   name: {
@@ -37,3 +29,33 @@ userSchema.virtual('name.full').set(function (value) {
 });
 
 exports.User = mongoose.model('User', userSchema);
+
+
+/*
+ * I’m sharing my credentials here.
+ * Feel free to use it while you’re learning.
+ * After that, create and use your own credentials.
+ * Thanks.
+ *
+ * MONGOLAB_URI=mongodb://express-mongo:65fe67162433087bca93aaa832e3bf56@dokku-mongo-express-mongo:27017/express-mongo
+ * 'mongodb://example:example@ds053312.mongolab.com:53312/todolist'
+ */
+
+mongoose.connect(process.env.MONGOLAB_URI, function (error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
+});
+
+function makeDefaultConnection() {
+  var uri = process.env.MONGOLAB_URI;
+
+  mongoose.connect(uri, {}, function(err, db){
+    if(err){
+      console.log('Connection Error ::: ', err);
+    } else {
+      console.log('Successfully Connected!');
+    }
+  });
+}
+
+module.exports.defaultConnection = makeDefaultConnection();
