271468582699c847e4d863f9b0b2d448e5e51311
Seth Bergman <seth.atxwebs@gmail.com>
2016-02-14 17:38:29 -0600

    testing mongoose connection reqs

--- db.js
+++ db.js
@@ -2,35 +2,6 @@ var express  = require('express'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser'),
 
-userSchema = new mongoose.Schema({
-  username: String,
-  gender: String,
-  name: {
-    title: String,
-    first: String,
-    last: String
-  },
-  location: {
-    street: String,
-    city: String,
-    state: String,
-    zip: Number
-  }
-});
-
-userSchema.virtual('name.full').get(function () {
-  return _.startCase(this.name.first + ' ' + this.name.last);
-});
-
-userSchema.virtual('name.full').set(function (value) {
-  var bits = value.split(' ');
-  this.name.first = bits[0];
-  this.name.last = bits[1];
-});
-
-exports.User = mongoose.model('User', userSchema);
-
-
 /*
  * I’m sharing my credentials here.
  * Feel free to use it while you’re learning.
@@ -59,3 +30,32 @@ function makeDefaultConnection() {
 }
 
 module.exports.defaultConnection = makeDefaultConnection();
+
+
+userSchema = new mongoose.Schema({
+  username: String,
+  gender: String,
+  name: {
+    title: String,
+    first: String,
+    last: String
+  },
+  location: {
+    street: String,
+    city: String,
+    state: String,
+    zip: Number
+  }
+});
+
+userSchema.virtual('name.full').get(function () {
+  return _.startCase(this.name.first + ' ' + this.name.last);
+});
+
+userSchema.virtual('name.full').set(function (value) {
+  var bits = value.split(' ');
+  this.name.first = bits[0];
+  this.name.last = bits[1];
+});
+
+exports.User = mongoose.model('User', userSchema);
