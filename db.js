7489618c277f766763c01eaa960dc8da35df4cdb
Seth Bergman <seth.atxwebs@gmail.com>
2016-02-14 10:31:34 -0600

    added custom mongo uri

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
-var uri = 'mongodb://localhost:27017/test';
+// var uri = 'mongodb://localhost:27017/test';
+var uri = 'mongodb://express-mongo:65fe67162433087bca93aaa832e3bf56@dokku-mongo-express-mongo:27017/express-mongo';
 
 var _ = require('lodash');
 var mongoose = require('mongoose');
