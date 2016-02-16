f7cfe2c3290444bd6532ccfc42ab8b4c98a91cf5
Seth Bergman <seth.atxwebs@gmail.com>
2016-02-15 19:52:33 -0600

    MONGODB=

--- db.js
+++ db.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-var uri = 'mongodb://express-mongo:65fe67162433087bca93aaa832e3bf56@dokku-mongo-express-mongo:27017/express-mongo';
-=======
-var uri = 'mongodb://localhost:27017/test';
->>>>>>> 05e69e7892a6bf23417c5ecee7214fe08507a83c
+//var uri = 'mongodb://express-mongo:65fe67162433087bca93aaa832e3bf56@dokku-mongo-express-mongo:27017/express-mongo';
 
 var _ = require('lodash');
 var mongoose = require('mongoose');
